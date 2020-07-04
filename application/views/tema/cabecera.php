b5cbac4051304e4faa4fe24720c261c6729ed646
git <johndoe@example.com>
2020-07-04 17:01:47 -0300

    nueva

--- application/views/tema/cabecera.php
+++ application/views/tema/cabecera.php
@@ -40,11 +40,6 @@
           <ul class="dropdown-menu">
             <li><a href=<?php echo site_url("usuarios/listadocompleto");?>>Listado de usuarios</a></li>
             <li><a href=<?php echo site_url("usuarios/nuevo");?>>Nuevo usuario</a></li>
-            <li><a href="#">Something else here</a></li>
-            <li role="separator" class="divider"></li>
-            <li><a href="#">Separated link</a></li>
-            <li role="separator" class="divider"></li>
-            <li><a href="#">One more separated link</a></li>
           </ul>
         </li>
         <li><a href="<?php echo site_url('notas/nuevanota');?>"><span class="glyphicon glyphicon-plus" aria-hidden="true"></span> Agregar nota</a></li>
@@ -53,9 +48,6 @@
         <li class="dropdown">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"> <span class="glyphicon glyphicon-user" aria-hidden="true"></span> <?php echo $this->session->userdata["usuario"]; ?> <span class="caret"></span></a>
           <ul class="dropdown-menu">
-            <li><a href="#">Action</a></li>
-            <li><a href="#">Another action</a></li>
-            <li><a href="#">Something else here</a></li>
             <li role="separator" class="divider"></li>
             <li><a href=<?php echo site_url("login/salir");?>>Cerrar sesión</a></li>
           </ul>
